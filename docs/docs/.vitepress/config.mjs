c0420687d9c9e0b96c7845f85ebbf5c63047b4a6
CODE GEAR <papiyadutta685@gmail.com>
2026-05-06 13:51:30 +0530

    Update config.mjs

--- docs/docs/.vitepress/config.mjs
+++ docs/docs/.vitepress/config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   description: "A Lightweight CSS Framework for Rapid Web Development",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    logo: '/logo.png',
+    logo: '/public/output-onlinepngtools-1.png',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Overview of ArtapaCSS', link: '/overview' },
