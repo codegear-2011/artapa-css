92203c6b2d1934c5d9f6c01916620c9ac28a406a
CODE GEAR <papiyadutta685@gmail.com>
2026-05-15 14:13:34 +0530

    Create Layout.vue

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout />
+</template>
+
+<style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css');
+</style>
